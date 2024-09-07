94a018a9767cee16c7f995aa5e68ef80ab2c88dc
Zohre Gurbanova <Zohre Gurbanova@DESKTOP-OQ4C5AH>
2024-09-07 11:47:45 +0400

    end commit

--- Employee.Domain/Entities/DepartmentEntity.cs
+++ Employee.Domain/Entities/DepartmentEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Employee.Domain.Entities
 {
     public class DepartmentEntity : BaseEntity
     {
+        [MaxLength(50)]
         public string Name { get; set; }
         public ICollection<EmployeeEntity> Employees { get; set; }
 
--- Employee.Domain/Entities/EmployeeEntity.cs
+++ Employee.Domain/Entities/EmployeeEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,10 @@ namespace Employee.Domain.Entities
 {
     public class EmployeeEntity : BaseEntity
     {
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [MaxLength(50)]
         public string Surname { get; set; }
         public DateTime BirthDate { get; set; }
         public int DepartmentId { get; set; }
